1ec782527ca4ccaab15e43bb44f7f0d14fe26bdc
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-09-17 14:05:17 -0400

    Update recipe.js
    updated js to make it a horizontal card instead of vertical

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -3,22 +3,28 @@ let savedFoodRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
 function init(){
     if(savedFoodRecipes){
         for (var i = 0; i < savedFoodRecipes.length; i++) {
-            let recipeCard = $('<div>').attr("class", "card");
-            let foodImg = $('<img>').attr("class", "card-img-top").attr("src", savedFoodRecipes[i].imgSrc).attr("style", "width: 20%");
+            let recipeCardHoriz = $('<div>').attr("class", "card mb-3");
+            let recipeCard = $('<div>').attr("class", "row no-gutters");
+            let imgDiv = $('<div>').attr("class", "col-md-4");
+            let foodImg = $('<img>').attr("class", "card-img").attr("src", savedFoodRecipes[i].imgSrc);
+            let summaryDiv = $('<div>').attr("class", "col-md-8");
             let recipeCardBody = $('<div>').attr("class", "card-body");
             let title = $('<h5>').attr("class", "card-title").text(savedFoodRecipes[i].title);
             let readyMin = $('<p>').attr("class", "card-text").text("Ready In: " + savedFoodRecipes[i].readyMin + " min");
             let servings = $('<p>').attr("class", "card-text").text("Servings: " + savedFoodRecipes[i].servings);
             
-            recipeCard.append(foodImg);
+            imgDiv.append(foodImg);
+            recipeCard.append(imgDiv);
             recipeCardBody.append(title);
             recipeCardBody.append(readyMin);
             recipeCardBody.append(servings);
             recipeCardBody.append($("<button>").attr("id", "showIngredientsBtn").attr("class", "btn btn-primary ingredientsBtn").attr("data-recipeid", savedFoodRecipes[i].recipeId).attr("data-toggle", "modal").attr("data-target", "#recipeModal").text("Show Ingredients"));
             recipeCardBody.append($('<br/>'))
             recipeCardBody.append($('<button>').attr("class", "btn btn-primary").attr("id", "deleteFromRecipeBook").text("Delete Recipe"));
-            recipeCard.append(recipeCardBody);
-            $("#foodRecipeDiv").append(recipeCard);
+            summaryDiv.append(recipeCardBody);
+            recipeCard.append(summaryDiv);
+            recipeCardHoriz.append(recipeCard);
+            $("#foodRecipeDiv").append(recipeCardHoriz);
         };
     }else{
         alert("no recipes")
