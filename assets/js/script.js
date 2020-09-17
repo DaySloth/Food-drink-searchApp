ec5581c1169ce733273dabe18ee7c78631acb37e
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-09-16 21:53:51 -0400

    updated food and js
    added checkboxes to food.html, added js to check for checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let recipeSearch;
+let allergies = [];
 
 
 function searchIngredients(recipeid) {
@@ -31,10 +32,19 @@ function searchIngredients(recipeid) {
 
 function searchForRecipe() {
     console.log(recipeSearch)
+    allergies = [];
+    let allergiesID = ["#dairy","#egg","#gluten","#peanut","#sesame","#seafood","#shellfish","#soy","#sulfite","#treeNut","#wheat"];
+
+    for(var i = 0; i < allergiesID.length; i++){
+        if($(allergiesID[i]).prop('checked')){
+            allergies.push($(allergiesID[i]).attr("value"));
+        }
+    };
+
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=" + recipeSearch,
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=" + recipeSearch + "&intolerances=" + allergies.toString(),
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -45,25 +55,30 @@ function searchForRecipe() {
     $.ajax(settings).done(function (response) {
         console.log(response);
         $("#recipeCardDiv").empty();
-        for (var i = 0; i < 5; i++) {
-            let recipeCard = $('<div>').attr("class", "card").attr("style", "width: 18rem");
-            let imageURL = "https://spoonacular.com/recipeImages/" + response.results[i].image;
-            let foodImg = $('<img>').attr("class", "card-img-top").attr("src", imageURL).attr("style", "width: inherited");
-            let recipeCardBody = $('<div>').attr("class", "card-body");
-            let title = $('<h5>').attr("class", "card-title").text(response.results[i].title);
-            let readyMin = $('<p>').attr("class", "card-text").text("Ready In: " + response.results[i].readyInMinutes + " min");
-            let servings = $('<p>').attr("class", "card-text").text("Servings: " + response.results[i].servings);
-            
-            recipeCard.append(foodImg);
-            recipeCardBody.append(title);
-            recipeCardBody.append(readyMin);
-            recipeCardBody.append(servings);
-            recipeCardBody.append($("<button>").attr("id", "showIngredientsBtn").attr("class", "btn btn-primary ingredientsBtn").attr("data-recipeid", response.results[i].id).attr("data-toggle", "modal").attr("data-target", "#recipeModal").text("Show Ingredients"));
-            recipeCardBody.append($('<br/>'))
-            recipeCardBody.append($('<button>').attr("class", "btn btn-primary").attr("id", "addToRecipeBook").text("Add to Recipe Book"));
-            recipeCard.append(recipeCardBody);
-            $("#recipeCardDiv").append(recipeCard);
+        if(response.results.length === 0){
+            alert("no results");
+        } else {
+            for (var i = 0; i < 5; i++) {
+                let recipeCard = $('<div>').attr("class", "card").attr("style", "width: 18rem");
+                let imageURL = "https://spoonacular.com/recipeImages/" + response.results[i].image;
+                let foodImg = $('<img>').attr("class", "card-img-top").attr("src", imageURL).attr("style", "width: inherited");
+                let recipeCardBody = $('<div>').attr("class", "card-body");
+                let title = $('<h5>').attr("class", "card-title").text(response.results[i].title);
+                let readyMin = $('<p>').attr("class", "card-text").text("Ready In: " + response.results[i].readyInMinutes + " min");
+                let servings = $('<p>').attr("class", "card-text").text("Servings: " + response.results[i].servings);
+                
+                recipeCard.append(foodImg);
+                recipeCardBody.append(title);
+                recipeCardBody.append(readyMin);
+                recipeCardBody.append(servings);
+                recipeCardBody.append($("<button>").attr("id", "showIngredientsBtn").attr("class", "btn btn-primary ingredientsBtn").attr("data-recipeid", response.results[i].id).attr("data-toggle", "modal").attr("data-target", "#recipeModal").text("Show Ingredients"));
+                recipeCardBody.append($('<br/>'))
+                recipeCardBody.append($('<button>').attr("class", "btn btn-primary").attr("id", "addToRecipeBook").text("Add to Recipe Book"));
+                recipeCard.append(recipeCardBody);
+                $("#recipeCardDiv").append(recipeCard);
+            };
         };
+        
     });
 };
 
