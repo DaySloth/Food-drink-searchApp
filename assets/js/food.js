b3640f574a09b977c317b4b4d5003c723c64258f
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-09-17 14:10:54 -0400

    Update food.js
    made the initial search cards be horizontal

--- assets/js/food.js
+++ assets/js/food.js
@@ -58,23 +58,29 @@ function searchForRecipe() {
             alert("no results");
         } else {
             for (var i = 0; i < 5; i++) {
-                let recipeCard = $('<div>').attr("class", "card").attr("style", "width: 18rem");
+                let recipeCardHoriz = $('<div>').attr("class", "card mb-3");
+                let recipeCard = $('<div>').attr("class", "row no-gutters");
                 let imageURL = "https://spoonacular.com/recipeImages/" + response.results[i].image;
-                let foodImg = $('<img>').attr("class", "card-img-top").attr("src", imageURL).attr("style", "width: inherited");
+                let foodImg = $('<img>').attr("class", "card-img-top").attr("src", imageURL).attr("style", "width: 18em");
+                let imgDiv = $('<div>').attr("class", "col-md-4");
+                let summaryDiv = $('<div>').attr("class", "col-md-8");
                 let recipeCardBody = $('<div>').attr("class", "card-body");
                 let title = $('<h5>').attr("class", "card-title").text(response.results[i].title);
                 let readyMin = $('<p>').attr("class", "card-text").text("Ready In: " + response.results[i].readyInMinutes + " min");
                 let servings = $('<p>').attr("class", "card-text").text("Servings: " + response.results[i].servings);
                 
-                recipeCard.append(foodImg);
+                imgDiv.append(foodImg);
+                recipeCard.append(imgDiv);
                 recipeCardBody.append(title);
                 recipeCardBody.append(readyMin);
                 recipeCardBody.append(servings);
                 recipeCardBody.append($("<button>").attr("id", "showIngredientsBtn").attr("class", "btn btn-primary ingredientsBtn").attr("data-recipeid", response.results[i].id).attr("data-toggle", "modal").attr("data-target", "#recipeModal").text("Show Ingredients"));
                 recipeCardBody.append($('<br/>'))
-                recipeCardBody.append($('<button>').attr("class", "btn btn-primary").attr("id", "addToRecipeBook").text("Add to Recipe Book"));
-                recipeCard.append(recipeCardBody);
-                $("#recipeCardDiv").append(recipeCard);
+                recipeCardBody.append($('<button>').attr("class", "btn btn-primary").attr("id", "deleteFromRecipeBook").text("Delete Recipe"));
+                summaryDiv.append(recipeCardBody);
+                recipeCard.append(summaryDiv);
+                recipeCardHoriz.append(recipeCard);
+                $("#recipeCardDiv").append(recipeCardHoriz);
             };
         };
     });
