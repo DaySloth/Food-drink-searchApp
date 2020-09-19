c7a7182763a09befbbd8a44f48cc08355039f478
Tahliyah Mims <mimst97@gmail.com>
2020-09-19 09:28:32 -0500

    Appends results in card
    needs to be formatted and styled still, ignore index changes

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -19,7 +19,10 @@ function DrinkSearch() {
     }
 
 
-    $.ajax({ url: drinksAPI, method: "GET",}).then(function (response) {
+    $.ajax({ 
+        url: drinksAPI, 
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
         let cardHoriz = $('<div>').attr("class", "card mb-3");
         let card = $('<div>').attr("class", "row no-gutters");
@@ -27,29 +30,29 @@ function DrinkSearch() {
         let Image = $('<img>').attr("class", "card-img-top")
         let imgDiv = $('<div>').attr("class", "col-md-4");
         let bodyDiv = $('<div>').attr("class", "col-md-8");
-        if (response.drinks.length === 0); {
-            noResults = $("<h5>No Results Found. Try a new search!</h5>");
-            resultsDispay.append(noResults);
+        // if (response.drinks.length === 0); {
+        //     noResults = $("<h5>No Results Found. Try a new search!</h5>");
+        //     resultsDispay.append(noResults);
            
-        }
-        if (response.drinks.length>0){ 
+        // }
+        // elseif (response.drinks.length>0); { 
             for (var i = 0; i < response.drinks.length; i++) {
                 let drinkPic = response.drinks[i].strDrinkThumb;
-                picture.attr("src", drinkPic).attr("style", "width: 18cm");
+                Image.attr("src", drinkPic).attr("style", "width: 18cm");
                 let drinkName = response.drinks[i].strDrink;
                 let explore = $("<button>Details</button>");
                 let drinkID = response.drinks[i].idDrink;
-                let newDiv = $("<div>");
-                Image.append(picture);
                 imgDiv.append(Image);
                 card.append(imgDiv);
-                cardBody.append(drinkName);
-                cardBody.append(explore);
+                bodyDiv.append(drinkName);
+                bodyDiv.append(explore);
+                cardBody.append(bodyDiv);
                 card.append(cardBody);
-                resultsDispay.append(card);
+                cardHoriz.append(card);
+                resultsDispay.append(cardHoriz);
                 //thinking of using this for checking ingredients to be able to flag allergens
                 return drinkID
-        }
+            // }
     }
     })
 }
