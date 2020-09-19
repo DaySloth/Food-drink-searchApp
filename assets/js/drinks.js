7691bd4c177e07d1b32f451ef6e8fc1fedd10e6b
Tahliyah Mims <mimst97@gmail.com>
2020-09-19 11:27:41 -0500

    fixed card format

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -1,10 +1,32 @@
 searchType = ""
 resultsDispay = $("#results")
 //function that gets ingredient details button
-// function getIngredients() {
-//     let instructions = 
+function IngredientDisplay() {
+    let modalTitle = $(this).parent().strDrink;
+    let ingredientsList = $('<ul>');
+    let ing1= $(this).parent().strIngredient1;
+    let newIng=$("<li>");
+    newIng.append(ing1);
+    ingredientsList.append(newIng);
+    // let ing2= $(this).parent().strIngredient2
+    // let ing3= $(this).parent().strIngredient3
+    // let ing4= $(this).parent().strIngredient4
+    // let ing5= $(this).parent().strIngredient5
+    // let ing6= $(this).parent().strIngredient6
+    // let ing7= $(this).parent().strIngredient7
+    // let ing8= $(this).parent().strIngredient8
+    // let ing9= $(this).parent().strIngredient9
+    let instructions= $(this).parent().strInstructions
 
-// }
+    console.log(newIng, ingredientsList, instructions)
+    
+
+    $('.modal-content').attr("style", "background-color: white; color: black");
+    $('#modalTitle').text(modalTitle);
+    $('#modalBody').html(ingredientsList);
+    $('#modalBody').append(instructions);
+
+}
 
 function DrinkSearch() {
 
@@ -19,41 +41,34 @@ function DrinkSearch() {
     }
 
 
-    $.ajax({ 
-        url: drinksAPI, 
+    $.ajax({
+        url: drinksAPI,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        let cardHoriz = $('<div>').attr("class", "card mb-3");
-        let card = $('<div>').attr("class", "row no-gutters");
-        let cardBody = $('<div>').attr("class", "card-body");
-        let Image = $('<img>').attr("class", "card-img-top")
-        let imgDiv = $('<div>').attr("class", "col-md-4");
-        let bodyDiv = $('<div>').attr("class", "col-md-8");
-        // if (response.drinks.length === 0); {
-        //     noResults = $("<h5>No Results Found. Try a new search!</h5>");
-        //     resultsDispay.append(noResults);
-           
-        // }
-        // elseif (response.drinks.length>0); { 
-            for (var i = 0; i < response.drinks.length; i++) {
-                let drinkPic = response.drinks[i].strDrinkThumb;
-                Image.attr("src", drinkPic).attr("style", "width: 18cm");
-                let drinkName = response.drinks[i].strDrink;
-                let explore = $("<button>Details</button>");
-                let drinkID = response.drinks[i].idDrink;
-                imgDiv.append(Image);
-                card.append(imgDiv);
-                bodyDiv.append(drinkName);
-                bodyDiv.append(explore);
-                cardBody.append(bodyDiv);
-                card.append(cardBody);
-                cardHoriz.append(card);
-                resultsDispay.append(cardHoriz);
-                //thinking of using this for checking ingredients to be able to flag allergens
-                return drinkID
-            // }
-    }
+
+        for (var i = 0; i < response.drinks.length; i++) {
+            let cardHoriz = $('<div>').attr("class", "card mb-3");
+            let card = $('<div>').attr("class", "row no-gutters");
+            let cardTitle = $("<h3 class= card-title>")
+            let cardBody = $('<div>').attr("class", "card-body");
+            let Image = $('<img>').attr("class", "card-img")
+            let imgDiv = $('<div>').attr("class", "col-md-4");
+            let bodyDiv = $('<div>').attr("class", "col-md-8");
+            let drinkPic = response.drinks[i].strDrinkThumb;
+            Image.attr("src", drinkPic)
+            let drinkName = response.drinks[i].strDrink;
+            let explore = $("<button class=btn btn-info id=ingredients>See Ingredients</button>");
+            imgDiv.append(Image);
+            card.append(imgDiv);
+            cardTitle.append(drinkName);
+            cardBody.append(cardTitle)
+            cardBody.append(explore);
+            bodyDiv.append(cardBody);
+            card.append(bodyDiv);
+            cardHoriz.append(card);
+            resultsDispay.append(cardHoriz);
+        }
     })
 }
 
@@ -66,7 +81,7 @@ $("#submitBtn").on("click", function (event) {
 $("#byName").on("click", function (event) {
     event.preventDefault();
     searchType = "Name"
-    
+
     return searchType
 })
 $("#byIngredient").on("click", function (event) {
@@ -75,3 +90,7 @@ $("#byIngredient").on("click", function (event) {
 
     return searchType
 })
+resultsDispay.on("click", "#ingredients", function(event) {
+    event.preventDefault();
+    IngredientDisplay($(this));
+});
